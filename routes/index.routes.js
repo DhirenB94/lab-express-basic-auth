cf46b6c51bbf45d2540ce795f9e6b77ff6bba9fc
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2021-04-22 11:53:56 +0100

    Lab done including bonuses

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+function requireLogin(req, res, next) {
+    if (req.session.currentUser) { //if logged in can continue
+      next();
+    } else {
+      res.redirect('/login') //otherwise redirect to login page
+    }
+  }
+
+
+
+
 /* GET home page */
 router.get('/', async (req, res, next) => {
-    res.render('index')
+    res.render('index', {user: req.session.currentUser})
 });
+
+
+
+router.get('/private', requireLogin, async (req, res) => {
+    res.render('private')
+})
+
+router.get('/main', requireLogin, async (req, res) => {
+    res.render('main')
+})
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/User.model");
 const bcrypt = require('bcryptjs');
 
+//Sign in 
 router.get('/signup', async (req, res) => {
     res.render('signup')
 });
@@ -36,5 +37,49 @@ router.post("/signup", async (req, res) => {
       return
     }
   });
+
+
+
+
+//Login
+router.get('/login', async (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', async (req, res) => {
+  const {username, password} = req.body
+  if (username === "" || password === "") {
+    res.render("login", {
+      errorMessage: "Indicate username and password",
+    }) ; 
+    return
+  }
+
+  const user = await User.findOne({username:username})
+  if(user === null) {
+    res.render("login", {
+    errorMessage: "Invalid Login",
+  }) ; 
+  return
+  }
+  if(bcrypt.compareSync(password, user.password)) {
+    req.session.currentUser = user;
+    res.redirect('/')
+  } else {
+    res.render("login", {
+      errorMessage: "Either the username or the password is incorrect",
+    }) ; 
+    return
+  }
+})
+
+
+
+//logout
+router.post('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
+});
+
 
 module.exports = router;
